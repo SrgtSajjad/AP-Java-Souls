7d327ecbba945bf75832c54df219825369d55bd8
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-12 10:26:47 +0330

    wizard class updated

--- src/main/java/sbu/cs/playerClasses/Wizard.java
+++ src/main/java/sbu/cs/playerClasses/Wizard.java
@@ -5,32 +5,41 @@ import sbu.cs.Monster;
 import sbu.cs.Player;
 
 // Implementing this class is optional
-public class Wizard extends Player
-{
+public class Wizard extends Player {
     int mana;
     int healSpellPower;
     int healSpellCost;
+    int damageSpellPower;
+    int damageSpellCost;
 
     public Wizard(String name, String role, int health, int attackPower) {
-        super(name, "Wizard", 600, 40);
+        super(name, "Wizard", 600, 30);
+        damageSpellPower = 50;
         healSpellPower = 100;
         healSpellCost = 20;
-        mana =5;
+        damageSpellCost = 5;
+        mana = 5;
     }
 
     public void castHealSpell() {
         if (getHealth() + healSpellPower > 600) {
             setHealth(600);
-        }
-        else {
+        } else {
             setHealth(getHealth() + healSpellPower);
         }
+        mana -= healSpellCost;
+    }
+
+    public void castDamageSpell(GameObject target) {
+        target.takeDamage(damageSpellPower);
+        mana -= damageSpellCost;
+        mana += 10;
     }
 
     @Override
     public void attack(GameObject target) {
         target.takeDamage(getAttackPower());
-        mana += 5;
+        mana += 15;
         System.out.println(getName() + " attacked the " + ((Monster) target).getRole() + " \"" + ((Monster) target).getName() + "\"");
     }
 
