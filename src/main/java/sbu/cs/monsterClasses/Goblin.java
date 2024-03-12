80681e901fccb4665708cf4a655ba0fe1ad9f9d8
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-12 23:24:24 +0330

    goblin class updated

--- src/main/java/sbu/cs/monsterClasses/Goblin.java
+++ src/main/java/sbu/cs/monsterClasses/Goblin.java
@@ -1,8 +1,34 @@
 package sbu.cs.monsterClasses;
 
-public class Goblin
-{
-    public Goblin() {
+import sbu.cs.GameObject;
+import sbu.cs.Player;
+import sbu.cs.Monster;
 
+
+public class Goblin extends Monster {
+
+
+    public Goblin(String name, String role, int health, int attackPower, boolean isAlive) {
+        super(name, "Goblin", 500, 40, true);
+
+    }
+
+    @Override
+    public void attack(GameObject target) {
+        target.takeDamage(getAttackPower());
+
+        System.out.println(getName() + " attacked the " + ((Player) target).getRole() + " \"" + ((Player) target).getName() + "\"");
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        setHealth(getHealth() - damage);
+    }
+
+    @Override
+    public void die() {
+        if (getHealth() <= 0) {
+            System.out.println(getName() + " the " + getRole() + " was killed");
+        }
     }
 }
