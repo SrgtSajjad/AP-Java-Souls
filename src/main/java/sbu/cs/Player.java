e455fa4117d60cbda38ec612e481d7b068c652fc
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-12 08:27:08 +0330

    getters & setters added to player class

--- src/main/java/sbu/cs/Player.java
+++ src/main/java/sbu/cs/Player.java
@@ -1,17 +1,48 @@
 package sbu.cs;
 
-public abstract class Player
+public abstract class Player implements GameObject
 {
-    String name;
-    int health;
-    int attackPower;
-    public Player(String name, int health, int attackPower) {
+    private String name;
+    private String role;
+
+    private int health;
+    private int attackPower;
+    public Player(String name, String role, int health, int attackPower) {
         this.name = name;
+        this.role = role;
         this.health = health;
         this.attackPower = attackPower;
-
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getAttackPower() {
+        return attackPower;
+    }
+
+    public void setAttackPower(int attackPower) {
+        this.attackPower = attackPower;
+    }
 }
