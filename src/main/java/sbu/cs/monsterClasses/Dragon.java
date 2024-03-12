d272c24cf280ba1b8001e58824d507ee911a3fbf
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-12 23:24:09 +0330

    dragon class updated

--- src/main/java/sbu/cs/monsterClasses/Dragon.java
+++ src/main/java/sbu/cs/monsterClasses/Dragon.java
@@ -1,9 +1,47 @@
 package sbu.cs.monsterClasses;
 
-public class Dragon
-{
-    public Dragon() {
+import sbu.cs.GameObject;
+import sbu.cs.Monster;
+import sbu.cs.Player;
 
+public class Dragon extends Monster {
+    int fireDamage;
+    int fireCharge;
+
+    public Dragon(String name) {
+        super(name, "Dragon", 1000, 50, true);
+        setName(name);
+        fireDamage = 100;
+        fireCharge = 3;
+    }
+
+    public void breathFire(GameObject target) {
+        target.takeDamage(fireDamage);
+        fireCharge = 0;
+        System.out.println(getName() + " breathed fire on the " + ((Player) target).getRole() + " \"" + ((Player) target).getName() + "\"");
+    }
+
+    @Override
+    public void attack(GameObject target) {
+        if (fireCharge == 3) {
+            breathFire(target);
+        } else {
+            target.takeDamage(getAttackPower());
+            fireCharge++;
+            System.out.println(getName() + " attacked the " + ((Player) target).getRole() + " \"" + ((Player) target).getName() + "\"");
+        }
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        setHealth(getHealth() - damage);
+    }
+
+    @Override
+    public void die() {
+        if (getHealth() <= 0) {
+            System.out.println(getName() + " the " + getRole() + " was killed");
+        }
     }
 }
 
