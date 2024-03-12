51cdbaada24eb00f96484f11b2cfd422c647f37f
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-12 08:28:48 +0330

    player class extended into Assassin class

--- src/main/java/sbu/cs/playerClasses/Assassin.java
+++ src/main/java/sbu/cs/playerClasses/Assassin.java
@@ -1,8 +1,29 @@
 package sbu.cs.playerClasses;
 
-public class Assassin
+import sbu.cs.GameObject;
+import sbu.cs.Player;
+
+public class Assassin extends Player
 {
-    public Assassin() {
+    public Assassin(String name, String role, int health, int attackPower) {
+        super(name, role, health, attackPower);
+        setRole("Assassin");
+    }
+
+
+    @Override
+    public void attack(GameObject target) {
+
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+
+    }
+
+    @Override
+    public void die() {
 
     }
 }
+
