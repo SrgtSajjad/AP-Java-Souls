f3c0260fe111e73d8ec8b47b5df159c8138ac55f
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-12 09:44:57 +0330

    Assassin class methods updated

--- src/main/java/sbu/cs/playerClasses/Knight.java
+++ src/main/java/sbu/cs/playerClasses/Knight.java
@@ -1,8 +1,38 @@
 package sbu.cs.playerClasses;
 
-public class Knight
-{
-    public Knight() {
+import sbu.cs.GameObject;
+import sbu.cs.Monster;
+import sbu.cs.Player;
 
+public class Knight extends Player {
+
+    int armor;
+
+    public Knight(String name, String role, int health, int attackPower) {
+        super(name, "Knight", 1000, 100);
+        armor = 5;
+    }
+
+    @Override
+    public void attack(GameObject target) {
+        target.takeDamage(getAttackPower());
+
+        System.out.println(getName() + " attacked the " + ((Monster) target).getRole() + " \"" + ((Monster) target).getName() + "\"");
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        if (armor > 0) {
+            setHealth(getHealth() - damage / armor);
+            armor -= 1;
+        } else
+            setHealth(getHealth() - damage);
+    }
+
+    @Override
+    public void die() {
+        if (getHealth() <= 0) {
+            System.out.println(getName() + " the " + getRole() + " was killed");
+        }
     }
 }
