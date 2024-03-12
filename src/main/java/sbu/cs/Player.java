48df9bcf47b80c45a5c9232f76b0ca61c5662d4c
SrgtSajjad <msajjad.za135@gmail.com>
2024-03-12 07:54:50 +0330

    Player class attributes and constructor done

--- src/main/java/sbu/cs/Player.java
+++ src/main/java/sbu/cs/Player.java
@@ -2,8 +2,16 @@ package sbu.cs;
 
 public abstract class Player
 {
-    public Player() {
+    String name;
+    int health;
+    int attackPower;
+    public Player(String name, int health, int attackPower) {
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
 
     }
+
+
 
 }
